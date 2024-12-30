c94c32e55376a5d1da8c191a9df7ae0267e35807
Maciej Michalski <5445923+spy86@users.noreply.github.com>
2024-12-30 13:19:02 +0000

    Update account.tf

--- account.tf
+++ account.tf
@@ -1,11 +1,13 @@
 resource "azurerm_cosmosdb_account" "main" {
-  name                      = "${var.environment}-${var.cosmosdb_account_name}db"
-  location                  = data.azurerm_resource_group.rg.location
-  resource_group_name       = data.azurerm_resource_group.rg.name
-  offer_type                = var.cosmosdb_account_offer_type
-  kind                      = var.cosmosdb_account_kind
-  tags                      = var.default_tags
-  enable_automatic_failover = var.cosmosdb_account_enable_automatic_failover
+  name                              = "${var.environment}-${var.cosmosdb_account_name}-cosmos"
+  location                          = data.azurerm_resource_group.rg.location
+  resource_group_name               = data.azurerm_resource_group.rg.name
+  offer_type                        = var.cosmosdb_account_offer_type
+  kind                              = var.cosmosdb_account_kind
+  tags                              = var.default_tags
+  automatic_failover_enabled        = var.cosmosdb_account_enable_automatic_failover
+  is_virtual_network_filter_enabled = var.is_virtual_network_filter_enabled
+  ip_range_filter                   = toset(var.ip_range_filter)
 
   capabilities {
     name = var.cosmosdb_account_capabilities_name
@@ -20,4 +22,11 @@ resource "azurerm_cosmosdb_account" "main" {
     location          = data.azurerm_resource_group.rg.location
     failover_priority = 0
   }
+
+  dynamic "virtual_network_rule" {
+    for_each = var.cosmosdb_virtual_network_rules != null ? var.cosmosdb_virtual_network_rules : []
+    content {
+      id = virtual_network_rule.value
+    }
+  }  
 }
